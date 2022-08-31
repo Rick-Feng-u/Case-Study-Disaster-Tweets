98ce93b50bc444b2dcf36d55aac22d82af9b7a34
Rick Feng <rickfeng99@gmail.com>
2022-08-31 11:11:55 -0700

    minor code refactoring

--- train_and_validation.py
+++ train_and_validation.py
@@ -1,5 +1,5 @@
 from util import training_data_cleaning, testing_data_cleaning, vectorized_data_and_padding, \
-    DisasterTweetsBidirectionalGRU, dataset, load_data
+    DisasterTweetsBidirectionalGRU, dataset, load_data, train, evaluation
 
 import torch
 import torch.nn as nn
@@ -16,8 +16,8 @@ if __name__ == "__main__":
 
     x_train, y_train, x_test, vocab = vectorized_data_and_padding(train_df, test_df)
 
-    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(x_train, y_train,
-                                                                        test_size=0.2, shuffle=True)
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(x_train, y_train,
+                                                                                    test_size=0.2, shuffle=True)
 
     input_size = len(vocab)
     hidden_size = 256
@@ -25,12 +25,13 @@ if __name__ == "__main__":
     train_dataset = dataset(X_train, Y_train)
     train_loader = load_data(train_dataset)
 
-    test_dataset = dataset(X_test, Y_test)
-    test_loader = load_data(test_dataset)
+    validation_dataset = dataset(X_validation, Y_validation)
+    validation_loader = load_data(validation_dataset)
 
     GRU_classfication_model = DisasterTweetsBidirectionalGRU(input_size, hidden_size)
 
     criterion = nn.BCELoss()
     optimizer = torch.optim.Adam(GRU_classfication_model.parameters(), lr=0.01)
 
-
+    train(train_loader, num_epochs, GRU_classfication_model, criterion, optimizer)
+    evaluation(validation_loader, GRU_classfication_model)
