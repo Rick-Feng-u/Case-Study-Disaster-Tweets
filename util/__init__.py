e52a47475515a2415926b6f4f0bbf8cbbe0fac0a
Rick Feng <rickfeng99@gmail.com>
2022-08-31 11:31:45 -0700

    test data function is now available

--- util/__init__.py
+++ util/__init__.py
@@ -2,4 +2,4 @@ from .data_prepossing import only_text, tokenized_clean_list
 from .data_processing_for_model import training_data_cleaning, testing_data_cleaning, vectorized_data_and_padding
 from .model import DisasterTweetsBidirectionalGRU
 from .custom_dataset import dataset, load_data
-from .train_validation_test_iterations import train, evaluation
+from .train_validation_test_iterations import train, evaluation, test_data_prediction
